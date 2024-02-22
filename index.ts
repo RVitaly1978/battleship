3eb55efd96c890f33bf3391c9184ab81d14a1137
Vital Raicheu <rvitaly1978@gmail.com>
2024-02-22 22:50:10 +0300

    feat: update index files

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import WebSocket from 'ws'
 import { startHttpServer } from './src/http_server'
 import { startWSS } from './src/ws_server'
 import { onProcessExit } from './src/ws_server/helpers'
@@ -7,7 +8,11 @@ const httpServer = startHttpServer()
 const wsServer = startWSS()
 
 onProcessExit(() => {
-  wsServer.clients.forEach(client => { client.close() })
+  wsServer.clients.forEach(client => {
+    if (client?.readyState === WebSocket.OPEN) {
+      client.close()
+    }
+  })
   wsServer.close()
   httpServer.close()
 })
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,16 +1,26 @@
 import { WebSocketServer } from 'ws'
-import { socketHandler } from './handler'
-import { getWSPortFromEnv, logWSServerStartedOnPort } from './helpers'
+import { connectionHandler, errorHandler, closeHandler } from './handlers/wssHandlers'
+import { getWSServerPortFromEnv, logWSServerStartedOnPort } from './helpers'
 
 export const startWSS = () => {
-  const wsPort = getWSPortFromEnv()
+  const wssPort = getWSServerPortFromEnv()
 
   const wss = new WebSocketServer(
-    { port: wsPort, clientTracking: true },
-    () => { logWSServerStartedOnPort(wsPort) },
+    { port: wssPort, clientTracking: true },
+    () => { logWSServerStartedOnPort(wssPort) },
   )
 
-  wss.on('connection', socketHandler)
+  wss.on('connection', (ws, request) => {
+    connectionHandler(ws, request)
+  })
+
+  wss.on('error', (error) => {
+    errorHandler(wssPort, error)
+  })
+
+  wss.on('close', () => {
+    closeHandler(wss, wssPort)
+  })
 
   return wss
 }
